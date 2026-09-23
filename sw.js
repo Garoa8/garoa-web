d49b8749a8d9ad66ad01a3609a23c92d1a2f4617
Garoa8 <hola@xn--lar-sxa.com>
2026-09-23 17:49:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'garoa-v50';
+const URLS = [
+  '/',
+  '/index.html',
+  '/lista-de-espera.html',
+  '/asesoria.html',
+  '/talleres.html',
+  '/calendario.html',
+  '/newsletter.html',
+  '/narciso.garoa.png',
+  '/garoa-flower.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
